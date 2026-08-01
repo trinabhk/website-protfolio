ade20d3b3bfce36238c544e2e0db13635a9db224
trinabhk <karkitrinabh30@gmail.com>
2026-08-02 00:26:50 +0545

    Replace GSC service account key with Workload Identity Federation

--- scripts/resubmit-sitemap.js
+++ scripts/resubmit-sitemap.js
@@ -4,14 +4,7 @@ const SITE_URL = 'https://trinabh.com.np/';
 const SITEMAP_URL = 'https://trinabh.com.np/sitemap.xml';
 
 async function main() {
-  const keyJson = process.env.GSC_SERVICE_ACCOUNT_KEY;
-  if (!keyJson) {
-    throw new Error('GSC_SERVICE_ACCOUNT_KEY environment variable is not set');
-  }
-
-  const credentials = JSON.parse(keyJson);
   const auth = new google.auth.GoogleAuth({
-    credentials,
     scopes: ['https://www.googleapis.com/auth/webmasters'],
   });
 
